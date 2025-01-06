71a3c0099d3e2dae27a3bec4af7b74f16e569c2a
joshuaatobra <X00230012@myTUDublin.ie>
2025-01-06 03:49:57 +0000

    api

--- frontend.py
+++ frontend.py
@@ -1,11 +1,14 @@
-
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
+api_key = os.getenv('ApiKey')
+
 @app.route('/')
 def form():
-    return '''
+    return f'''
         <html>
             <body>
                 <h2>Enter Your Details</h2>
@@ -14,6 +17,7 @@ def form():
                     Email: <input type="email" name="email"><br><br>
                     <input type="submit" value="Submit">
                 </form>
+                <p><strong>API Key:</strong> {api_key if api_key else "Not Configured"}</p>
             </body>
         </html>
     '''
@@ -22,7 +26,8 @@ def form():
 def submit():
     name = request.form['name']
     email = request.form['email']
-    return jsonify({"name": name, "email": email})
+    return jsonify({"name": name, "email": email, "api_key": api_key})
 
 if __name__ == '__main__':
     app.run(debug=True)
+
